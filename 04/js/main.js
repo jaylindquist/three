1684217dac4bfe4f512b3c452e386a1a6f457fe4
jay.lindquist <jay.lindquist@code42.com>
2014-05-22 21:39:30 -0500

    Create three.04 files

---
+++ 04/js/main.js
@@ -0,0 +1,62 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var triangleMesh;
+var squareMesh;
+
+drawScene();
+render();
+
+function render() {
+	requestAnimationFrame(render);
+
+	triangleMesh.rotation.y -= 0.05;
+	squareMesh.rotation.x += 0.1;
+
+	renderer.render(scene, camera);
+}
+
+function drawScene() {
+	var triangleGeometry = new THREE.Geometry();
+	triangleGeometry.vertices.push(new THREE.Vector3(0, 1, 0));
+	triangleGeometry.vertices.push(new THREE.Vector3(-1, -1, 0));
+	triangleGeometry.vertices.push(new THREE.Vector3(1, -1, 0));
+	triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
+	
+	triangleGeometry.faces[0].vertexColors[0] = new THREE.Color(0xFF0000);
+	triangleGeometry.faces[0].vertexColors[1] = new THREE.Color(0x00FF00);
+	triangleGeometry.faces[0].vertexColors[2] = new THREE.Color(0x0000FF);
+		
+	var triangleMat = new THREE.MeshBasicMaterial({
+		vertexColors:THREE.VertexColors,
+		side:THREE.DoubleSide
+	});
+	
+	triangleMesh = new THREE.Mesh(triangleGeometry, triangleMat);
+	triangleMesh.position.set(-1.5, 0, -6);
+
+	scene.add(triangleMesh);
+	
+	var squareGeometry = new THREE.Geometry();
+	squareGeometry.vertices.push(new THREE.Vector3(-1, 1, 0));
+	squareGeometry.vertices.push(new THREE.Vector3(-1, -1, 0));
+	squareGeometry.vertices.push(new THREE.Vector3(1, -1, 0));
+	squareGeometry.vertices.push(new THREE.Vector3(1, 1, 0));
+	squareGeometry.faces.push(new THREE.Face3(0, 1, 2));
+	squareGeometry.faces.push(new THREE.Face3(0, 2, 3));
+	
+	var squareMat = new THREE.MeshBasicMaterial({
+		color:0x8080FF,
+		side:THREE.DoubleSide
+	});
+	
+	squareMesh = new THREE.Mesh(squareGeometry, squareMat);
+	squareMesh.position.set(1.5, 0, -6);
+	
+	scene.add(squareMesh);
+	
+}
